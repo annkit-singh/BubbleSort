579cc725b94a8fdeca534d9beace8695f3e401a4
Aankit Singh <52100296+annkit-singh@users.noreply.github.com>
2020-10-01 00:14:09 +0530

    Update bubbleSORT.java

--- bubbleSORT.java
+++ bubbleSORT.java
@@ -5,6 +5,29 @@
  * @author Arl Kheem Rey Galario
  */
 public class bubbleSORT {
+  
+ 
+//this is the optimized version of bubble sort
+    public void bubbleSort(int array[]) {
+        int count = 0;  // counts the number of comparisons in an array while traversing 
+        for (int i = 0; i < array.length; i++) {
+            count = 0;
+
+            for (int j = 0; j < array.length - 1; j++) {
+                step++;
+                if (array[j] > array[j + 1]) {
+                    count++;
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+                if (j == array.length - 2 && count == 0)  //if no comparisons found then we should return as array is sorted at that point
+                    return;
+
+            }
+
+        }
+    }
  
     
     public static void main(String [] args){
